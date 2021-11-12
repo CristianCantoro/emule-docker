66abb414b5ff239712b25b609b34457a625abbe4
Cristian Consonni <cristian@balist.es>
2021-11-12 21:35:18 +0100

    Fix execution with UID != 0

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# shellcheck disable=SC2039
 
 if [ -f "/data/download" ]; then
     echo "Creating download directory..."
@@ -10,12 +11,12 @@ if [ -f "/data/tmp" ]; then
     mkdir -p /data/tmp
 fi
 
-if [ $UID != "0" ]; then
+if [ "$UID" != "0" ]; then
     echo "Fixing permissions..."
-    useradd --shell /bin/bash -u ${UID} -U -d /app -s /bin/false emule && \
+    useradd --shell /bin/bash -u "${UID}" -U -d /app emule
     usermod -G users emule
-    chown -R ${UID}:${GID} /data
-    chown -R ${UID}:${GID} /app
+    chown -R "${UID}":"${GID}" /data
+    chown -R "${UID}":"${GID}" /app
 fi
 
 echo "Applying configuration..."
@@ -34,4 +35,13 @@ echo "Waiting to run emule... 2"
 sleep 5
 echo "Waiting to run emule... 1"
 sleep 5
-/usr/bin/wine /app/emule.exe
+if [ "$UID" != "0" ]; then
+    # run as another user
+    echo "run as user 'emule'"
+    su -c '/usr/bin/wine /app/emule.exe' emule
+
+else
+    # run as root
+    /usr/bin/wine /app/emule.exe
+
+fi
